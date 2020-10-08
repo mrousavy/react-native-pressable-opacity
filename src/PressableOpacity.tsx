2259850ab9cbe6f007080adae1aa5decc2087f70
Marc Rousavy <marcrousavy@hotmail.com>
2020-10-08 12:00:19 +0200

    Create PressableOpacity component

---
+++ src/PressableOpacity.tsx
@@ -0,0 +1,47 @@
+import React, { useCallback } from 'react';
+import { PressableProps, Pressable, PressableStateCallbackType, StyleProp, ViewStyle } from 'react-native';
+
+export interface PressableOpacityProps extends PressableProps {
+	/**
+	 * The opacity to use when `disabled={true}`
+	 *
+	 * @default 0.3
+	 */
+	disabledOpacity?: number;
+	/**
+	 * The opacity to animate to when the user presses the button
+	 *
+	 * @default 0.2
+	 */
+	activeOpacity?: number;
+}
+
+export type StyleType = (state: PressableStateCallbackType) => StyleProp<ViewStyle>;
+
+export default function PressableOpacity({
+	style,
+	disabled = false,
+	disabledOpacity = 0.3,
+	activeOpacity = 0.2,
+	...passThroughProps
+}: PressableOpacityProps): React.ReactElement {
+	const getOpacity = useCallback(
+		(pressed: boolean) => {
+			if (disabled) {
+				return disabledOpacity;
+			} else {
+				if (pressed) return activeOpacity;
+				else return 1;
+			}
+		},
+		[activeOpacity, disabled, disabledOpacity],
+	);
+	const _style = useCallback<StyleType>(({ pressed }) => [style as ViewStyle, { opacity: getOpacity(pressed) }], [getOpacity, style]);
+
+	return <Pressable style={_style} disabled={disabled} {...passThroughProps} />;
+}
+
+// TODO: REMOVE PLACEHOLDER PressableOpacity
+// export default function PressableOpacity(props: TouchableOpacityProps & { children?: React.ReactNode }): React.ReactElement {
+// 	return <TouchableOpacity delayPressIn={0} {...props} />;
+// }
