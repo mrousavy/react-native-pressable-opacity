26a4e75dd7321a98d35ec5c389baad93c67f7242
Marc Rousavy <marcrousavy@hotmail.com>
2021-10-28 10:12:14 +0200

    Allow disabling

--- src/NativePressableOpacity.tsx
+++ src/NativePressableOpacity.tsx
@@ -2,18 +2,25 @@ import React, { useMemo } from 'react';
 import type { ViewProps } from 'react-native';
 import { TapGestureHandler, TapGestureHandlerGestureEvent } from 'react-native-gesture-handler';
 import Reanimated, {
-	cancelAnimation,
-	delay,
+	withDelay,
 	Easing,
 	useAnimatedGestureHandler,
 	useAnimatedStyle,
 	useSharedValue,
 	withTiming,
+	useDerivedValue,
+	WithTimingConfig,
 } from 'react-native-reanimated';
 import { PRESSABLE_IN_LIST_DELAY } from './Constants';
 
-export interface NativePressableOpacityProps extends ViewProps, Reanimated.WithTimingConfig {
+export interface NativePressableOpacityProps extends ViewProps, WithTimingConfig {
 	children: React.ReactNode;
+	/**
+	 * The opacity to use when `disabled={true}`
+	 *
+	 * @default 0.3
+	 */
+	disabledOpacity?: number;
 	/**
 	 * The opacity to use when the Pressable is being pressed.
 	 * @default 0.2
@@ -47,27 +54,42 @@ export interface NativePressableOpacityProps extends ViewProps, Reanimated.WithT
  * A Pressable that lowers opacity when pressed. Uses the native responder system from react-native-gesture-handler instead of the JS Pressability API.
  */
 export function NativePressableOpacity(props: NativePressableOpacityProps): React.ReactElement {
-	const { activeOpacity = 0.2, isInList, duration = 50, easing = Easing.linear, disabled = false, ref, style, onPress, ...passThroughProps } = props;
+	const { activeOpacity = 0.2, isInList, duration = 50, easing = Easing.linear, disabled = false, disabledOpacity = 0.3, ref, style, onPress, ...passThroughProps } = props;
 
-	const opacity = useSharedValue(1);
+	const isPressed = useSharedValue(false)
+	const timingConfig = useMemo<WithTimingConfig>(() => ({ duration, easing }), [duration, easing]);
+
+	const opacity = useDerivedValue(() => {
+		if (disabled) {
+			return withTiming(disabledOpacity, timingConfig)
+		} else {
+			if (isPressed.value) {
+				if (isInList) {
+					return withDelay(PRESSABLE_IN_LIST_DELAY, withTiming(activeOpacity, timingConfig))
+				} else {
+					return withTiming(activeOpacity, timingConfig)
+				}
+			} else {
+				return withTiming(1, timingConfig)
+			}
+		}
+	}, [disabled, disabledOpacity, timingConfig, isPressed, isInList, activeOpacity])
+
 	const animatedStyle = useAnimatedStyle(() => ({ opacity: opacity.value }), [opacity]);
-	const timingConfig = useMemo<Reanimated.WithTimingConfig>(() => ({ duration, easing }), [duration, easing]);
 
 	const onGestureEvent = useAnimatedGestureHandler<TapGestureHandlerGestureEvent>(
 		{
 			onStart: () => {
-				cancelAnimation(opacity);
-				opacity.value = isInList ? delay(PRESSABLE_IN_LIST_DELAY, withTiming(activeOpacity, timingConfig)) : withTiming(activeOpacity, timingConfig);
+				isPressed.value = true
 			},
 			onEnd: () => {
 				onPress();
 			},
 			onFinish: () => {
-				cancelAnimation(opacity);
-				opacity.value = withTiming(1, timingConfig);
+				isPressed.value = false
 			},
 		},
-		[opacity, isInList, activeOpacity, timingConfig, onPress],
+		[isPressed, onPress],
 	);
 
 	return (
